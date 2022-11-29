c3f5bb620d7a4bc2aa2105cef501960c0413085b
Rustem Sankubayev <rustem.sankubayev@alumni.nu.edu.kz>
2022-11-29 11:59:24 +0600

    git init

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Main.bubbleSort(new int[]{4,3,5,1,2,});
+    }
+
+    public static int[] bubbleSort(int[] arr){
+        int tmp;
+        for (int i=0; i<arr.length-1; i++){
+            for (int j=0; j<arr.length-i-1; j++){
+                if (arr[j]>arr[j+1]){
+                    tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        return arr;
+    }
+}
